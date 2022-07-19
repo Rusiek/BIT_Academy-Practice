8ea099af3ed2e0de6fe762abddd96090e9d71ac2
yoinker57 <93473651+yoinker57@users.noreply.github.com>
2022-07-19 22:22:40 +0200

    Update test_add.py

--- Pytest/Task3/test_add.py
+++ Pytest/Task3/test_add.py
@@ -2,3 +2,12 @@ from cmplex import C
 
 # Napisać testy sprawdzające dodawanie liczb zespolonych
 # w załączonym pliku
+
+
+def test_0():
+    x = C(1, 2)
+    y = C(2, 3)
+    assert C.__add__(x, y) == (3, 5)
+
+
+

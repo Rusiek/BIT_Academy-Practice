e23fb588287d30e3c55e24100682ebc8f0f59398
kkkucia <11.karolinakucia@gmail.com>
2022-07-22 16:00:57 +0200

    Task1 done

--- Pytest/Task1/test_function.py
+++ Pytest/Task1/test_function.py
@@ -1,2 +1,14 @@
 from function import square
 # Napisać cztery testy z dla funckji square dla 0, 1, -1 i 100
+
+def test_one():
+    assert square(0) == 0 ** 2
+
+def test_two():
+    assert square(1) == 1 ** 2
+
+def test_three():
+    assert square(-1) == (-1) ** 2 
+
+def test_four():
+    assert square(100) == 100 ** 2

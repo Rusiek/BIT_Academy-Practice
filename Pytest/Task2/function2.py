ee94098a1cb147a5b6df2bfec4545f2a3d401be4
Karolina Kucia <11.karolinakucia@gmail.com>
2022-07-23 12:25:22 +0200

    Task2 done

--- Pytest/Task2/function2.py
+++ Pytest/Task2/function2.py
@@ -1,9 +1,14 @@
 def is_prime_correct(x):
-    if x == 2:
+    if x == 2 or x == 3:
         return True
-    elif x < 2 or x % 2 == 0 :
+    if x <= 1 or not x % 2 or not x % 3:
         return False
-    for i in range(3, int(x**0.5)+1):
-        if x % i == 0:
+    i = 5
+    while i < int(x**0.5) + 1:
+        if not x % i:
             return False
+        i += 2
+        if not x % i:
+            return False
+        i += 4
     return True

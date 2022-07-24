260f448e38bd7690d4ddc2f13593be0621fcc215
Dominik Adamczyk <dominik.adamczyk1000@gmail.com>
2022-07-24 15:08:26 +0200

    TestMini class done

--- Pytest/Task2/test_class.py
+++ Pytest/Task2/test_class.py
@@ -3,12 +3,28 @@ from function import is_prime
 class TestMini:
     # Napisać testy sprawdzające funkcję is_prime dla liczb
     # z przedziału <-1, 6>
+    def test_one(self):
+        assert is_prime(-1) == True
+    def test_two(self):
+        assert is_prime(0) == False
+    def test_three(self):
+        assert is_prime(1) == False
+    def test_four(self):
+        assert is_prime(2) == True
+    def test_five(self):
+        assert is_prime(3) == True
+    def test_six(self):
+        assert is_prime(4) == False
+    def test_seven(self):
+        assert is_prime(5) == True
+    def test_eight(self):
+        assert is_prime(6) == False
 
-class TestMid:
-    # Napisać 5 testów sprawdzających funkcję is_prime dla losowych
-    # liczb z przedziału <10 ** 4, 10 ** 5>
+# class TestMid:
+#     # Napisać 5 testów sprawdzających funkcję is_prime dla losowych
+#     # liczb z przedziału <10 ** 4, 10 ** 5>
 
-class TestMaxi:
+# class TestMaxi:
     # Napisać 5 testów sprawdzających funkcję is_prime dla losowych
     # z przedziału <10 ** 12, 10 ** 13>, a następnie
     # naprawić funkcję is_prime aby testy wykonywały się szybciej

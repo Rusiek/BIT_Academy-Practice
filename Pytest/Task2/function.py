c75e40d103f23b18b56b94b0e6af507c95b31e66
Julija <juleczek2002@gmail.com>
2022-07-24 10:35:29 +0200

    Better second function

--- Pytest/Task2/function.py
+++ Pytest/Task2/function.py
@@ -1,7 +1,14 @@
 def is_prime(x):
     if x == 0 or x == 1:
         return False
-    for i in range(2, x):
-        if not x % i:
+    if x == 2:
+        return True
+    if x % 2 == 0:
+        return False
+
+    i = 3
+    while i * i <= x:
+        if x % i == 0:
             return False
-    return True
+        i += 1
+    return True 

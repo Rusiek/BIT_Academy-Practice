4b88d1ed6f32b6161d8b8801cb01dbb394832d2c
Dominik Adamczyk <dominik.adamczyk1000@gmail.com>
2022-07-24 15:21:31 +0200

    Done Task2

--- Pytest/Task2/test_class.py
+++ Pytest/Task2/test_class.py
@@ -1,5 +1,6 @@
 from function import is_prime
-
+from fast_alg import fast_prime
+from random import randint
 class TestMini:
     # Napisać testy sprawdzające funkcję is_prime dla liczb
     # z przedziału <-1, 6>
@@ -20,12 +21,45 @@ class TestMini:
     def test_eight(self):
         assert is_prime(6) == False
 
-# class TestMid:
-#     # Napisać 5 testów sprawdzających funkcję is_prime dla losowych
-#     # liczb z przedziału <10 ** 4, 10 ** 5>
+class TestMid:
+    # Napisać 5 testów sprawdzających funkcję is_prime dla losowych
+    # liczb z przedziału <10 ** 4, 10 ** 5>
+    def test_one(self):
+        i = randint(10 ** 4, 10 ** 5)
+        assert is_prime(i) == fast_prime(i)
+    def test_two(self):
+        i = randint(10 ** 4, 10 ** 5)
+        assert is_prime(i) == fast_prime(i)
+    def test_three(self):
+        i = randint(10 ** 4, 10 ** 5)
+        assert is_prime(i) == fast_prime(i)
+    def test_four(self):
+        i = randint(10 ** 4, 10 ** 5)
+        assert is_prime(i) == fast_prime(i)
+    def test_five(self):
+        i = randint(10 ** 4, 10 ** 5)
+        assert is_prime(i) == fast_prime(i)
+    
 
-# class TestMaxi:
+class TestMaxi:
     # Napisać 5 testów sprawdzających funkcję is_prime dla losowych
     # z przedziału <10 ** 12, 10 ** 13>, a następnie
     # naprawić funkcję is_prime aby testy wykonywały się szybciej
     # niż dla aktualnej funkcji
+    def test_one(self):
+        i = randint(10 ** 12, 10 ** 13)
+        assert is_prime(i) == fast_prime(i)
+    def test_two(self):
+        i = randint(10 ** 12, 10 ** 13)
+        assert is_prime(i) == fast_prime(i)
+    def test_three(self):
+        i = randint(10 ** 12, 10 ** 13)
+        assert is_prime(i) == fast_prime(i)
+    def test_four(self):
+        i = randint(10 ** 12, 10 ** 13)
+        assert is_prime(i) == fast_prime(i)
+    def test_five(self):
+        i = randint(10 ** 12, 10 ** 13)
+        print(i)
+        assert is_prime(i) == fast_prime(i)    
+

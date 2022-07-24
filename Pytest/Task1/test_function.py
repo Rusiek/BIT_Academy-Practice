73108600acf6a7919c127291b3d1a23c569f87b7
Julija <juleczek2002@gmail.com>
2022-07-24 08:54:34 +0200

    First task commit

--- Pytest/Task1/test_function.py
+++ Pytest/Task1/test_function.py
@@ -1,2 +1,14 @@
 from function import square
 # Napisać cztery testy z dla funckji square dla 0, 1, -1 i 100
+
+def test_x():
+    assert square(1) == 1
+
+def test_y():
+    assert square(0) == 0
+
+def tesy_z():
+    assert square(-1) == 1
+
+def test_p():
+    assert square(100) == 10000

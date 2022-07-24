7bb76e1f042186f0fc8a7dc8b28b853420609730
Julija <juleczek2002@gmail.com>
2022-07-24 09:21:54 +0200

    Function is prime commit

--- Pytest/Task2/function.py
+++ Pytest/Task2/function.py
@@ -1,7 +1,19 @@
 def is_prime(x):
     if x == 0 or x == 1:
         return False
-    for i in range(2, x):
-        if not x % i:
+    if  x == 2:
+        return True
+    
+    i = 2
+    while i * i <= x:
+        if x % i == 0:
             return False
+        i+=1
     return True
+
+
+
+
+
+
+    

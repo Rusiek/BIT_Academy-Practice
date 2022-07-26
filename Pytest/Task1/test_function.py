4963ca1ebd77297d6adb1eb46b77c44df4bd0ecf
KubaBiz <1kubi1@wp.pl>
2022-07-26 14:05:32 +0200

    Done Task 1

--- Pytest/Task1/test_function.py
+++ Pytest/Task1/test_function.py
@@ -1,2 +1,10 @@
 from function import square
 # Napisać cztery testy z dla funckji square dla 0, 1, -1 i 100
+def test_1():
+    assert square(0) == 0
+def test_2():
+    assert square(1) == 1
+def test_3():
+    assert square(-1) == 1
+def test_4():
+    assert square(100) == 10000

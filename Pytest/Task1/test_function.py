fc4eda1df9428757e1a82fab536699087cc45b67
Michał Kobiera <kobiera@zoho.com>
2022-07-27 17:25:40 +0200

    task1

--- Pytest/Task1/test_function.py
+++ Pytest/Task1/test_function.py
@@ -1,2 +1,10 @@
 from function import square
 # Napisać cztery testy z dla funckji square dla 0, 1, -1 i 100
+
+def test_square():
+    assert square(0) == 0
+    assert square(1) == 1
+    assert square(-1) == 1
+    assert square(100) == 10000
+
+test_square()

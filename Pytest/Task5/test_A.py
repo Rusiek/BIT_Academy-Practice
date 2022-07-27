cda89912772b1bd74fd524b4792ba5908c2c48bd
ksew1 <karol.sewilo@gmail.com>
2022-07-27 12:53:58 +0200

    Task5

--- Pytest/Task5/test_A.py
+++ Pytest/Task5/test_A.py
@@ -6,3 +6,14 @@ from function import count_chars
 
 # Funkcja count_chars zwraca trzyelementową tablicę
 # która zawiera odpowiednio liczbę wystąpień znaku 'a', 'b' i 'c'
+
+
+class Test1:
+    A = [('aabbc', [2, 2, 1]),
+         ('abac', [2, 1, 1]),
+         (('bbbbb'), [0, 5, 0]),
+         (('abcba', [2, 2, 1]))]
+
+    @pytest.mark.parametrize('s,sol', A)
+    def test_A(self, s, sol):
+        assert count_chars(s) == sol

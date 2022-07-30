f47b27e8fa20b78f1dce6f12f7a74b68fa56153e
Julija <juleczek2002@gmail.com>
2022-07-30 22:33:50 +0200

    Robot first part commit

--- OOP/Task1/robot.py
+++ OOP/Task1/robot.py
@@ -8,7 +8,10 @@ BATTERY_VAL = 10
 class robot:
     # mapa, x, y, bateria
     def __init__(self, T, x, y, b):
-        pass
+        self.__T = T
+        self.__x = x
+        self.__y = y
+        self.__b = b
 
     def left(self, val = 1):
         pass
@@ -20,12 +23,33 @@ class robot:
         pass
 
     def get_status(self):
-        pass
+        if self.get_battery() == 0:
+            return robotStatus.DEAD
+
+        T = self.__T
+        x = self.get_x()
+        y = self.get_y()
+
+        if T[x][y] == "T":
+            return robotStatus.ALIVE
+        elif T[x][y] == "W":
+            return robotStatus.WATER
+        else:
+            return robotStatus.CRASH
+
+
+
+
+
+
     def get_battery(self):
-        pass
+        return self.__b
+
     def get_map(self):
         pass
+
     def get_x(self):
-        pass
+        return self.__x
+        
     def get_y(self):
-        pass
+        return self.__y

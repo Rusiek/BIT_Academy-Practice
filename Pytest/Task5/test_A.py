5efa0092eab7b514bc6c30d6ad4c91b16bf6dad5
krzyswys <krzyswys3@gmail.com>
2022-08-01 17:28:41 +0200

    zad5

--- Pytest/Task5/test_A.py
+++ Pytest/Task5/test_A.py
@@ -1,6 +1,10 @@
 import pytest
 from function import count_chars
 
+@pytest.mark.parametrize("num, output",[("aabbcc",[2,2,2]),("aaabbcccccc",[3,2,6]),("aaabbbbbc",[3,5,1]),("aaaacc",[4,0,2])])
+def test_multiplication_11(num, output):
+   assert count_chars(num) == output
+
 # Napisać testy dla funkcji count_chars za pomocą
 # markera "parametrize"
 

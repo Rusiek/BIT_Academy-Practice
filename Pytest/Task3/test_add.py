7538db5496de235b33fbf62e9d0efe6789619897
krzyswys <krzyswys3@gmail.com>
2022-08-01 16:13:23 +0200

    zad3

--- Pytest/Task3/test_add.py
+++ Pytest/Task3/test_add.py
@@ -1,4 +1,17 @@
 from cmplex import C
 
+def test_1():
+    val=C(2,4).__add__(C(1,2))
+    assert val.x==3 and val.y==6
+def test_2():
+    val=C(90,0).__add__(C(1,0))
+    assert val.x==91 and val.y==0
+def test_3():
+    val=C(-11,25).__add__(C(34,-2543525))
+    assert val.x==23 and val.y==-2543500
+def test_4():
+    val=C(5,20).__add__(C(-24,40))
+    assert val.x==-19 and val.y==60
+
 # Napisać testy sprawdzające dodawanie liczb zespolonych
 # w załączonym pliku
--- Pytest/Task3/test_sub.py
+++ Pytest/Task3/test_sub.py
@@ -1,4 +1,15 @@
 from cmplex import C
-
+def test_13():
+    val=C(1,2).__sub__(C(1,2))
+    assert val.x==0 and val.y==0
+def test_14():
+    val=C(11,-2).__sub__(C(10,2))
+    assert val.x==1 and val.y==-4
+def test_15():
+    val=C(1245,-124).__sub__(C(45,1))
+    assert val.x==1200 and val.y==-125
+def test_16():
+    val=C(-9213,8235).__sub__(C(77,-55))
+    assert val.x==-9290 and val.y==8290
 # Napisać testy sprawdzające odejmowanie liczb zespolonych
 # w załączonym pliku

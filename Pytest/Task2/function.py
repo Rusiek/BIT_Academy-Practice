bd38190fddfc7def6b9df090b1b40d91f1a5600a
krzyswys <krzyswys3@gmail.com>
2022-08-01 14:36:31 +0200

    zad2

--- Pytest/Task2/function.py
+++ Pytest/Task2/function.py
@@ -1,7 +1,8 @@
+from math import sqrt
 def is_prime(x):
-    if x == 0 or x == 1:
+    if x == 0 or x == 1 or x<0:
         return False
-    for i in range(2, x):
-        if not x % i:
+    for i in range(2, int(sqrt(x))+1):
+        if  x % i==0:
             return False
     return True

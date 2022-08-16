a87fe22901709a9bf46143b430cca4e97e70a7bf
Szymon <szynon.oz@gmail.com>
2022-08-16 11:18:42 +0200

    Add tests with mark parametrize

--- Pytest/Task5/test_A.py
+++ Pytest/Task5/test_A.py
@@ -6,3 +6,9 @@ from function import count_chars
 
 # Funkcja count_chars zwraca trzyelementową tablicę
 # która zawiera odpowiednio liczbę wystąpień znaku 'a', 'b' i 'c'
+
+test_array = [("", [0, 0, 0]), ("abc", [1, 1, 1]), ("aaa", [3, 0, 0]), ("accacccccac", [3, 0, 8]), ("abccbabbbcaaccab", [5, 6, 5])]
+
+@pytest.mark.parametrize("test_input, expected", test_array)
+def test(test_input, expected):
+    assert count_chars(test_input) == expected
